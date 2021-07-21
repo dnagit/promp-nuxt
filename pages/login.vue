5efbe5e36c60f7c697f61a52130f10a6c882f865
mpprom <79315451+dnagit@users.noreply.github.com>
2021-07-21 23:36:05 +0700

    Update login.vue

--- pages/login.vue
+++ pages/login.vue
@@ -12,7 +12,7 @@
                     <li class="signup-inactive"><nuxt-link
             class="btn"
             :to="'register'"
-          >สมัครสมาชิก</nuxt-link></li>
+          >สมัครสมาชิกa</nuxt-link></li>
                 </ul>
             </div>
             <div ng-app ng-init="checked = false">
